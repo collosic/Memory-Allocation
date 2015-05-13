8a3626953d3d5a8792d462fa3a16d2800b1db670
shinmc <tommy9828@gmail.com>
2015-05-12 22:42:57 -0700

    Create map.c

---
+++ map.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int index;
+    char *bp;
+    struct node * next;
+} node_t;
+
+int insert_node(node_t * head, int index, char *bp) {
+    node_t * current = head;
+
+    while (current->next != NULL) {
+        current = current->next;
+    }
+
+    /* now we can add a new variable */
+    current->next = malloc(sizeof(node_t));
+    current->next->index = index;
+    current->next->bp = bp;
+    current->next->next = NULL;
+    return index;
+}
+
+char * find_node(node_t * head, int index){
+    node_t * current = head;
+
+    while (current != NULL) {
+        if (current->index == index){
+            return current->bp;
+        }
+        current = current->next;
+    }
+    return NULL;
+}
+
+int remove_by_index(node_t ** head, int n) {
+    int i = 0;
+    int retval = -1;
+    node_t * current = *head;
+    node_t * temp_node = NULL;
+
+    if (n == 0) {
+        return pop(head);
+    }
+
+    for (int i = 0; i < n-1; i++) {
+        if (current->next == NULL) {
+            return -1;
+        }
+        current = current->next;
+    }
+
+    temp_node = current->next;
+    retval = temp_node->index;
+    current->next = temp_node->next;
+    free(temp_node);
+
+    return retval;
+}
+
+void print_list(node_t * head) {
+    node_t * current = head;
+
+    while (current != NULL) {
+        printf("key %d\n", current->index);
+        printf("value %d\n", current->bp);
+        current = current->next;
+    }
+}
+
+int pop(node_t ** head) {
+    int retval = -1;
+    node_t * next_node = NULL;
+
+    if (*head == NULL) {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->index;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
