8a95305702323513a4f34acf08dcd5756bf5dd63
shinmc <tommy9828@gmail.com>
2015-05-18 10:03:47 -0700

    Update map.h

--- map.h
+++ map.h
@@ -1,15 +1,12 @@
-#ifndef MAP
-#define MAP
-
-
 typedef struct node {
     int index;
     char *bp;
+    int allocated_size;
     int written_heap_size;
     struct node * next;
 } node_t;
 
-int insert_node(node_t * head, int index, char *bp);
+int insert_node(node_t * head, int index, int allocated_size, char *bp);
 int remove_by_index(node_t * head, int n);
 node_t * find_node(node_t * head, int index);
 void print_list(node_t * head);
